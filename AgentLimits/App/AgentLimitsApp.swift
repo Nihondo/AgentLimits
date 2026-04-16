4716cc5468107c01063db49d10c30b9db1b235a8
Nihondo <nihondo@mac.com>
2026-04-16 23:47:01 +0900

    fix: プロジェクトバージョン,URLを最新化

--- AgentLimits/App/AgentLimitsApp.swift
+++ AgentLimits/App/AgentLimitsApp.swift
@@ -542,7 +542,7 @@ private struct MenuBarContentView: View {
 
     private func makeAboutCredits() -> NSAttributedString {
         let copyright = resolveAboutCopyright()
-        let repositoryURLString = "https://desireforwealth.com/products/agentlimits.shtml"
+        let repositoryURLString = "https://products.desireforwealth.com/products/agentlimits"
         let creditsText = "\(copyright)\nWebsite: \(repositoryURLString)"
         let attributed = NSMutableAttributedString(string: creditsText)
         let linkRange = (creditsText as NSString).range(of: repositoryURLString)
